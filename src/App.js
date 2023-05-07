9aa4ea6904ebd12e69eceba5bc266abd0f179e39
KKbeckang <beckang@bu.edu>
2023-05-07 00:34:20 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { DataGrid,GridCellParams, GridToolbar  } from '@mui/x-data-grid';
 
-function App() {
+const rows  = [
+  { id: 1, Condition: 'Rest', Duration: '60' },
+  { id: 2, Condition: 'Discharge', Duration: 'N/A' },
+  { id: 3, Condition: 'Rest', Duration: '30' },
+  { id: 4, Condition: 'Charge', Duration: '0.1' },
+  { id: 5, Condition: 'Rest', Duration: '5' },
+];
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 150},
+  { field: 'Condition', headerName: 'Condition', width: 150,editable: true},
+  { field: 'Duration', headerName: 'Duration', width: 150,editable: true },
+];
+
+ function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: '100%', width: '100%' }}>
+      <DataGrid rows={rows} columns={columns} slots={{
+    toolbar: GridToolbar,
+  }}
+/>
     </div>
   );
 }
+
 
 export default App;
